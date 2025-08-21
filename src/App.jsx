f0c7a40ac30cc0e5135f705e6ca1241d4f8baa31
Dguminski12 <davidguminski12@gmail.comgit config --global user.namegit config --global user.email davidguminski12@gmail.com>
2025-08-21 16:20:29 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>Jammming</h1>
+      <p>Search Spotify, build a playlist, save to your account.</p>
+    </main>
+  );
+}
